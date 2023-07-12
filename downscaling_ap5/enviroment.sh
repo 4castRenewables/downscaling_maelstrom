48fea7d82196e9e25ed085fb9765249c93510dfb
saragrau4 <sara.grau@4-cast.de>
2023-07-12 13:10:03 +0200

    Move main_train script to mlproject folder

--- downscaling_ap5/enviroment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export MANTIK_UNICORE_USERNAME=saragrau
-export MANTIK_UNICORE_PASSWORD=Elcorteingles00@@
-export MANTIK_UNICORE_PROJECT=mlproject
-export MLFLOW_TRACKING_URI=https://cloud.mantik.ai/
-export MANTIK_USERNAME=saragrau
-export MANTIK_UNICORE_PASSWORD=Elcorteingles00@@
-export MANTIK_PASSWORD=Elcorteingles00@@
