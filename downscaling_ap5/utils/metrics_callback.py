48094ec8af838b4236b534e4839b21465f0b3fa7
saragrau4 <sara.grau@4-cast.de>
2023-08-07 15:09:40 +0200

    debug metric callback

--- downscaling_ap5/utils/metrics_callback.py
+++ downscaling_ap5/utils/metrics_callback.py
@@ -8,14 +8,16 @@ class MetricsHistory(Callback):
         self.validation_loss= dict()
 
     def on_epoch_end(self, epoch, logs={}):
-        try:
-            self.training_loss[f"training loss epoch {epoch}"] = logs.get('loss')
-        except KeyError:
-            self.training_loss[f"training loss epoch {epoch}"] = logs.get('recon_loss')
-
-        try:
-            self.validation_loss[f"validation loss epoch {epoch}"] = logs.get('val_loss')
-        except KeyError:
-            self.validation_loss[f"validation loss epoch {epoch}"] = logs.get('val_recon_loss')
-
-        mlflow.log_metrics({**self.training_loss, **self.validation_loss})
+        # try:
+        #     self.training_loss[f"training loss epoch {epoch}"] = logs.get('loss')
+        # except KeyError:
+        #     self.training_loss[f"training loss epoch {epoch}"] = logs.get('recon_loss')
+        #
+        # try:
+        #     self.validation_loss[f"validation loss epoch {epoch}"] = logs.get('val_loss')
+        # except KeyError:
+        #     self.validation_loss[f"validation loss epoch {epoch}"] = logs.get('val_recon_loss')
+        #
+        # mlflow.log_metrics({**self.training_loss, **self.validation_loss})
+        keys = list(logs.keys())
+        print("End epoch {} of training; got log keys: {}".format(epoch, keys))
