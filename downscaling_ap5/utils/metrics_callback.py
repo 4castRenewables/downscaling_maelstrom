7ca05fb2056cf01fa3109ba4f8043fe57ea57b29
saragrau4 <sara.grau@4-cast.de>
2023-08-07 15:23:56 +0200

    metrics_callback debbuging

--- downscaling_ap5/utils/metrics_callback.py
+++ downscaling_ap5/utils/metrics_callback.py
@@ -3,21 +3,28 @@ import mlflow
 
 
 class MetricsHistory(Callback):
-    def on_train_begin(self, logs={}):
+    def on_train_begin(self, logs=None):
         self.training_loss = dict()
         self.validation_loss= dict()
 
-    def on_epoch_end(self, epoch, logs={}):
-        # try:
-        #     self.training_loss[f"training loss epoch {epoch}"] = logs.get('loss')
-        # except KeyError:
-        #     self.training_loss[f"training loss epoch {epoch}"] = logs.get('recon_loss')
-        #
-        # try:
-        #     self.validation_loss[f"validation loss epoch {epoch}"] = logs.get('val_loss')
-        # except KeyError:
-        #     self.validation_loss[f"validation loss epoch {epoch}"] = logs.get('val_recon_loss')
-        #
-        # mlflow.log_metrics({**self.training_loss, **self.validation_loss})
+    def on_epoch_end(self, epoch, logs=None):
         keys = list(logs.keys())
         print("End epoch {} of training; got log keys: {}".format(epoch, keys))
+
+        print(
+            "The average loss for epoch {} is {:7.2f} ".format(
+                epoch, logs["recon_loss"]
+            )
+        )
+
+        try:
+            self.training_loss[f"training loss epoch {epoch}"] = logs["loss"]
+        except KeyError:
+            self.training_loss[f"training loss epoch {epoch}"] = logs["recon_loss"]
+
+        try:
+            self.validation_loss[f"validation loss epoch {epoch}"] = logs["val_loss"]
+        except KeyError:
+            self.validation_loss[f"validation loss epoch {epoch}"] = logs["val_recon_loss"]
+
+        mlflow.log_metrics({**self.training_loss, **self.validation_loss})
